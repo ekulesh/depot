dad95a8897f161e7cc2b9e0c97dc184088893a0c
Eugene Kulesh <ekulesh@beltechsoftware.com>
2016-03-24 16:30:19 +0300

    p14.1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313140625) do
+ActiveRecord::Schema.define(version: 20160324131030) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 20160313140625) do
     t.decimal  "price",                     precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.string   "password_digest", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   add_foreign_key "line_items", "carts"
